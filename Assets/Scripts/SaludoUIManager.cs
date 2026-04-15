c0d917f5f755ec7e4dc036e859420a780eba1761
Martincho250 <50048880@est.ort.edu.ar>
2026-04-15 14:27:10 -0300

    Initial commit

---
+++ Assets/Scripts/SaludoUIManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SaludoUIManager : MonoBehaviour
+{
+    public TextMeshProUGUI txtSaludo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtSaludo.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
